4d9e068c71d29039c4951a0f60b0c68db2eb6603
Jules Grillot <jgrillot@biotope.fr>
2023-07-30 22:43:52 +0200

    minor changes before deposit

--- bd_topo_extractor/__about__.py
+++ bd_topo_extractor/__about__.py
@@ -75,7 +75,7 @@ __summary__: str = "{}\n{}".format(
     __plugin_md__.get("general").get("about"),
 )
 
-__title__: str = "{0}".format(__plugin_md__.get("general").get("name"))
+__title__: str = "{0}".format(__plugin_md__.get("wfs").get("plugin_name"))
 __title_clean__: str = "".join(e for e in __title__ if e.isalnum())
 
 __uri_homepage__: str = __plugin_md__.get("general").get("homepage")
@@ -105,7 +105,7 @@ if __name__ == "__main__":
     assert isinstance(plugin_md, dict)
     assert (
         "{0}".format(
-            __plugin_md__.get("general").get("name"),
+            __plugin_md__.get("wfs").get("plugin_name"),
         )
         == __title__
     )
